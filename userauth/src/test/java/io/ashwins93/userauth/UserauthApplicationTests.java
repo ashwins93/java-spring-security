7529c64595716f18f59f5f91513d3cbd550353d8
Ashwin <aswinjeganath@gmail.com>
2020-04-10 21:46:27 +0530

    project set up

---
+++ userauth/src/test/java/io/ashwins93/userauth/UserauthApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ashwins93.userauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
